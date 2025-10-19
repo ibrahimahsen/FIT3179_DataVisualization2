07ee527248845c9e18807f812d5ed593213c0f1b
ibrahimahsen <mibrahimahsen007@gmail.com>
2025-10-19 16:38:37 +1100

    Commit 7

--- vegalite_vs.js
+++ vegalite_vs.js
@@ -3,3 +3,4 @@ vegaEmbed("#choropleth", "visualizations/visualization1.vl.json").catch(console.
 vegaEmbed("#bar",        "visualizations/visualization3.vl.json").catch(console.error);
 vegaEmbed("#hotspots",   "visualizations/visualization2.vl.json").catch(console.error);
 vegaEmbed("#line", "visualizations/visualization4.vl.json").catch(console.error);
+vegaEmbed('#steam', "visualizations/visualization5.vl.json").catch(console.error);
