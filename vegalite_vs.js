bf8ef697e402f3d71e1b32da4c5ea04cfdf284e3
ibrahimahsen <mibrahimahsen007@gmail.com>
2025-10-19 11:57:37 +1100

    Commit 6

--- vegalite_vs.js
+++ vegalite_vs.js
@@ -1,4 +1,5 @@
 // Embed the three specs
-vegaEmbed("#choropleth", "visualizations/visualization1.json").catch(console.error);
-vegaEmbed("#bar",        "visualizations/visualization3.json").catch(console.error);
-vegaEmbed("#hotspots",   "visualizations/visualization2.json").catch(console.error);
+vegaEmbed("#choropleth", "visualizations/visualization1.vl.json").catch(console.error);
+vegaEmbed("#bar",        "visualizations/visualization3.vl.json").catch(console.error);
+vegaEmbed("#hotspots",   "visualizations/visualization2.vl.json").catch(console.error);
+vegaEmbed("line", "visualizations/visualization4.vl.json").catch(console.error);
