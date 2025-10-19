0561acc8c62af41dbe39209deb74f3203d0c4733
ibrahimahsen <mibrahimahsen007@gmail.com>
2025-10-19 12:37:33 +1100

    Added 3rd data domain

--- vegalite_vs.js
+++ vegalite_vs.js
@@ -2,4 +2,4 @@
 vegaEmbed("#choropleth", "visualizations/visualization1.vl.json").catch(console.error);
 vegaEmbed("#bar",        "visualizations/visualization3.vl.json").catch(console.error);
 vegaEmbed("#hotspots",   "visualizations/visualization2.vl.json").catch(console.error);
-vegaEmbed("line", "visualizations/visualization4.vl.json").catch(console.error);
+vegaEmbed("#line", "visualizations/visualization4.vl.json").catch(console.error);
